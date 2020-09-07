c2d3ec8dc5cbcb5a2fe0f8d4aafd835e477c92bb
Celiart <celiart.office@gmail.com>
2020-09-07 12:41:38 +0300

    add validate text to all inputs

--- js/validate-email.js
+++ js/validate-email.js
@@ -1,28 +1,136 @@
 function validateName() {
-    var name = document.getElementById('name').value;
-    if (name.length == 0) {
-        alert("Completează numele.");
-        return false;
+    if (document.body.contains(document.getElementById('name'))) {
+        var name = document.getElementById('name').value;
+        if (name.length == 0) {
+            alert("Completează numele si prenumele.");
+            return false;
 
+        }
+        if (!name.match(/^[a-zA-Z]{3,}(?: [a-zA-Z]+){0,2}$/)) {
+            alert("Introdu un nume si prenume valid!"); //Validation Message
+            return false;
+        }
     }
-    if (!name.match(/^[a-zA-Z]{3,}(?: [a-zA-Z]+){0,2}$/)) {
-        alert("Introdu un nume valid!"); //Validation Message
-        return false;
-    }
+
     return true;
 }
 
-function validateNameTwo() {
-    var name = document.getElementById('nameTwo').value;
-    if (name.length == 0) {
-        alert("Completează numele.");
-        return false;
+function validateFirstName() {
+    if (document.body.contains(document.getElementById('FirstName'))) {
+        var name = document.getElementById('FirstName').value;
+        if (name.length == 0) {
+            alert("Completează prenumele.");
+            return false;
 
+        }
+        if (!name.match(/^[a-zA-Z]{3,}(?: [a-zA-Z]+){0,2}$/)) {
+            alert("Introdu un prenume valid!"); //Validation Message
+            return false;
+        }
     }
-    if (!name.match(/^[a-zA-Z]{3,}(?: [a-zA-Z]+){0,2}$/)) {
-        alert("Introdu un nume valid!"); //Validation Message
-        return false;
+
+    return true;
+}
+
+function validateLastName() {
+    if (document.body.contains(document.getElementById('LastName'))) {
+        var name = document.getElementById('LastName').value;
+        if (name.length == 0) {
+            alert("Completează numele.");
+            return false;
+
+        }
+        if (!name.match(/^[a-zA-Z]{3,}(?: [a-zA-Z]+){0,2}$/)) {
+            alert("Introdu un nume valid!"); //Validation Message
+            return false;
+        }
     }
+
+    return true;
+}
+
+function validateStreet() {
+    if (document.body.contains(document.getElementById('Street'))) {
+        var name = document.getElementById('Street').value;
+        if (name.length == 0) {
+            alert("Completează numele străzii.");
+            return false;
+
+        }
+        // if (!name.match(/^[a-zA-Z]{3,}(?: [a-zA-Z]+){0,2}$/)) {
+        //     alert("Introdu o stradă validă!"); //Validation Message
+        //     return false;
+        // }
+    }
+
+    return true;
+}
+
+function validateCity() {
+    if (document.body.contains(document.getElementById('City'))) {
+        var name = document.getElementById('City').value;
+        if (name.length == 0) {
+            alert("Completează orașul.");
+            return false;
+
+        }
+        if (!name.match(/^[a-zA-Z]{3,}(?: [a-zA-Z]+){0,2}$/)) {
+            alert("Introdu un oraș valid!"); //Validation Message
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function validateState() {
+    if (document.body.contains(document.getElementById('State'))) {
+        var name = document.getElementById('State').value;
+        if (name.length == 0) {
+            alert("Completează județul.");
+            return false;
+
+        }
+        if (!name.match(/^[a-zA-Z]{3,}(?: [a-zA-Z]+){0,2}$/)) {
+            alert("Introdu un județ valid!"); //Validation Message
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function validateZipCode() {
+    if (document.body.contains(document.getElementById('ZipCode'))) {
+        var name = document.getElementById('ZipCode').value;
+        if (name.length == 0) {
+            alert("Completează codul poștal.");
+            return false;
+
+        }
+        if (!name.match(/^[a-zA-Z]{3,}(?: [a-zA-Z]+){0,2}$/)) {
+            alert("Introdu un cod poștal valid!"); //Validation Message
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function validateMessage() {
+    if (document.body.contains(document.getElementById('message'))) {
+        var name = document.getElementById('message').value;
+        if (name.length == 0) {
+            alert("Completează mesajul.");
+            return false;
+
+        }
+        if (!name.match(/^[a-zA-Z]{3,}(?: [a-zA-Z]+){0,2}$/)) {
+            alert("Introdu un nume si prenume valid!"); //Validation Message
+            return false;
+        }
+    }
+
     return true;
 }
 
@@ -39,44 +147,40 @@ function validatePhone() {
     }
 
     return true;
-
 }
 
 function validateEmail() {
+    if (document.body.contains(document.getElementById('email'))) {
+        var email = document.getElementById('email').value;
+        if (email.length == 0) {
+            alert("Email nu poate fi un câmp vid."); //Validation Message
+            return false;
 
-    var email = document.getElementById('email').value;
-    if (email.length == 0) {
-        alert("Email nu poate fi un câmp vid."); //Validation Message
-        return false;
-
-    }
-
-    if (!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
-        alert("Introdu adresa corectă de email!"); //Validation Message
-        return false;
-
+        }
+        if (!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+            alert("Introdu adresa corectă de email!"); //Validation Message
+            return false;
+        }
     }
 
     return true;
-
 }
 
 function validateForm() {
-    if (!validateName() || !validateEmail()) {
+    if (!validateName() || !validateFirstName() || !validateLastName() || !validateEmail() || !validateStreet() || !validateCity() || !validateState() || !validateMessage() || !validateZipCode()) {
+        //alert("Form not submitted");
+        //Validation Message
+        return false;
 
-    //   alert("Form not submitted");
-      //Validation Message
-      return false;
-    }
-    else {
-        if(typeof sendMessage === "function"){
+    } else {
+        if (typeof sendMessage === "function") {
             sendMessage();
         }
 
-        if(typeof sendOrder === "function"){
+        if (typeof sendOrder === "function") {
             sendOrder();
         }
-      submitted=true;
-      return true;
-    }    
-  }
+        submitted = true;
+        return true;
+    }
+}
