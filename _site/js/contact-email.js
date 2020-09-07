c0b3be0c648c7550c0ffda4a77239e7d6013bf17
Celiart <celiart.office@gmail.com>
2020-09-07 03:45:20 +0300

    edit contact and checkout source for email sender

--- _site/js/contact-email.js
+++ _site/js/contact-email.js
@@ -2,6 +2,7 @@ function sendMessage() {
     const name = document.getElementsByName('name')[0].value;
     const email = document.getElementsByName('email')[0].value;
     const message = document.getElementsByName('message')[0].value;
+    // const body = "Nume: " . name . ", Mesaj: " . message;
 
     Email.send({
         Host: "smtp.gmail.com",
@@ -9,7 +10,7 @@ function sendMessage() {
         Password: "Student3302499",
         To: 'ligia.bodnaras@student.usv.ro',
         From: email,
-        Subject: "Contact",
+        Subject: "Contact Celiart Formular",
         Body: message,
     }).then(
         res => alert("Mesaj trimis")
